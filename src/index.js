c7dacad17abdc172050ba41f909a42a6845408a3
Thomas Fresneau <thom.fresneau@gmail.com>
2017-11-29 15:01:14 +0100

    Configured Lint in start and watch scripts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import numeral from 'numeral'
 
 const courseValue = numeral(1000).format('$0,0.00');
 
-debugger;
+console.log(`Would pay ${courseValue} for this course`);// eslint-disable-line no-console
 
-console.log(`Would pay ${courseValue} for this course`);
