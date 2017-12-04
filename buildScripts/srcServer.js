b025007abbc0f3d74af48cefd51178e05f00a00c
Thomas Fresneau <thom.fresneau@gmail.com>
2017-12-04 15:36:36 +0100

    Setup an API to retrieve a list of users Configured a mocked list using JSON schema faker / faker served by JSON server

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+  res.json([
+    {"id": 1, "firstName": "Thomas", "lastName": "Fresneau", email:"tf@gmail.com"},
+    {"id": 2, "firstName": "Johanna", "lastName": "Stansfield", email:"js@gmail.com"},
+    {"id": 2, "firstName": "Chloe", "lastName": "Fresneau", email:"cf@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err){
   if(err){
     console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import numeral from 'numeral'
+import {getUsers} from './api/userApi';
 
-const courseValue = numeral(1000).format('$0,0.00');
+// Populate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
 
-console.log(`Would pay ${courseValue} for this course`);// eslint-disable-line no-console
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
 
+  global.document.getElementById('users').innerHTML = usersBody;
+
+});
